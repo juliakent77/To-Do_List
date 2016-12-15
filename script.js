874b8bb06d9bfa180b65a23dc0627bb7e54a316c
juliakent77 <juliakent@Julias-MacBook-Air.local>
2016-12-15 13:40:05 -0800

    Styling to avoid javascript

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 var myForm = document.getElementById('myForm');
 var container = document.getElementById('container');
 var myInput = document.getElementById('inputShit');
-var checkBox = document.createElement("checkbox");
 
-// checkBox.type = "checkbox";
-
-// container.appendChild(checkBox);
 
 myForm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -13,15 +9,5 @@ myForm.addEventListener('submit', function(event) {
   var pTag = document.createElement('p');
   pTag.innerHTML = inputValue;
   container.appendChild(pTag);
-  // container.appendChild(checkbox);
   event.target.inputShit.value = '';
 });
-
-// addButton.addEventListener("click", addTask);
-
-var check = document.querySelector('checkbox');
-list.addEventListener('click', function(event) {
-  if (event.target.tagName === 'LI') {
-    event.target.classList.toggle('checked');
-  }
-}, false);
