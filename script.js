592eed6f46cb37ea0b83590e207df7ea257e8cca
juliakent77 <juliakent@Julias-MacBook-Air.local>
2016-12-19 15:01:54 -0800

    Trying to get the checkboxes working, and failing miserably

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var myForm = document.getElementById('myForm');
 var container = document.getElementById('container');
 var myInput = document.getElementById('inputShit');
 
+var checkBox = document.createElement('checkbox');
+
+
 
 myForm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -11,3 +14,13 @@ myForm.addEventListener('submit', function(event) {
   container.appendChild(pTag);
   event.target.inputShit.value = '';
 });
+
+
+myForm.addEventListener('submit', function(event) {
+  var checkBox = document.createElement('checkBox');
+  container.appendChild(checkBox);
+  event.target.checkBox.value = "input[type=checkbox]";
+});
+
+
+// this is not working.... :(
